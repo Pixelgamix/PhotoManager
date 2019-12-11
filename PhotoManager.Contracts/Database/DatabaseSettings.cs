3b334fa04ddc0db0f8ee13dc4eed927a7631524d
Ingo <skarimba@googlemail.com>
2019-12-11 15:45:11 +0100

    created data-access tests.

--- PhotoManager.Contracts/Database/DatabaseSettings.cs
+++ PhotoManager.Contracts/Database/DatabaseSettings.cs
@@ -8,16 +8,16 @@ namespace PhotoManager.Contracts.Database
         /// <summary>
         /// Path, where the photos have to be stored. 
         /// </summary>
-        public string StoragePath { get; set; }
+        public virtual string StoragePath { get; set; }
 
         /// <summary>
         /// Format for the photo-filename.
         /// </summary>
-        public FileNameSettings FileNameSettings { get; set; }
+        public virtual FileNameSettings FileNameSettings { get; set; }
         
         /// <summary>
         /// Format for the directory-structure.
         /// </summary>
-        public string DirectoryFormat { get; set; }
+        public virtual string DirectoryFormat { get; set; }
     }
 }
